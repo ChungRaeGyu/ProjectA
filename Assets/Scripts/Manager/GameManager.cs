41dadbee45148bda88bc40a054c71444ea76215e
Chung Rae Gyu <wogns9769@gmail.com>
2025-01-14 21:25:41 +0900

    Update : GameManager.cs
    랜덤 위치를 정했습니다.
    각자 위치에 오브젝트 생성하게 만들기

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -22,65 +23,55 @@ public class GameManager : MonoBehaviourPunCallbacks
     PhotonView pv;
     //UI
     [SerializeField] TMP_Text roomNameTxt;
-    [SerializeField] TMP_Text peopleNumTxt;
     //PlayerObject
     GameObject character;
     //Ready
-    [SerializeField] Camera TopViewCamera; //choice seat
-    [SerializeField] Transform[] seats;
-    Dictionary<Player, int> seatNum = new Dictionary<Player, int>();
-
+    [SerializeField] Transform[] seats; //자리의 위치를 가지고 있다.
+    Dictionary<int, Player> seatNum = new Dictionary<int, Player>();
+    [SerializeField] List<int> turnList = new List<int>();
     [SerializeField] TMP_Text test;
     private void Awake()
     {
         pv = GetComponent<PhotonView>();
+        if (PhotonNetwork.IsMasterClient)
+        {
+            foreach (Player player in PhotonNetwork.PlayerList)
+            {
+                int random = RandomNum();
+                turnList.Add(random);
+                seatNum.Add(random, player);
+            }
+            ListSort(turnList);
+            pv.RPC("SeetTurnList", RpcTarget.AllBuffered, turnList.ToArray());
+        }
+
     }
     void Start()
     {
         roomNameTxt.text = $"방 이름 : {PhotonNetwork.CurrentRoom.Name}";
-        character = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity); //Resourse 풀더안에 있어야한다.
-        pv.RPC("RandomPlacement", RpcTarget.AllBuffered, PhotonNetwork.LocalPlayer, RandomNum());
-        UpdatePlayerList();
+        //character = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity); //Resourse 풀더안에 있어야한다.
+        //pv.RPC("RandomPlacement", RpcTarget.AllBuffered, PhotonNetwork.LocalPlayer, RandomNum());
     }
     void Update()
     {
         Test();
-    }    
+    }
     private void Test()
     {
-        string a="";
+        string a = "";
         foreach (var player in seatNum) {
             a += player;
         }
         test.text = a;
         a = "";
     }
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        //데이터 베이스가 있다면 이걸 받아오면 딱인데 나중에 넣을 건데
-        UpdatePlayerList();
-        //자 잠깐만 이거봐봐 들어오면 공유해야지
-    }
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        UpdatePlayerList();
-    }
-    #region UI
 
-    private void UpdatePlayerList()
-    {
-        peopleNumTxt.text = $"{PhotonNetwork.PlayerList.Length}명";
-    }
-    #endregion
 
     #region Ready
     [PunRPC]
-    private void RandomPlacement(Player player,int num)
+    private void SeetTurnList(int[] tempList)
     {
-        //Start에서 사용 됌 자리 랜덤 설정 후 Dictionary에 추가 
-        seatNum.Add(player, num);
-        character.transform.position = seats[seatNum[player]].transform.position;
-
+        turnList = tempList.ToList();
     }
 
     private int RandomNum()
@@ -89,11 +80,48 @@ public class GameManager : MonoBehaviourPunCallbacks
         do
         {
             rand = Random.Range(0, seats.Length);
-
-        } while (seatNum.ContainsValue(rand));
+            Debug.Log("랜덤 뽑기");
+        } while (seatNum.ContainsKey(rand));
 
         return rand;
         //Todo: seatNum에 넣어준다. 그리고 저 정보들을 끝나고 한대 모은다?
+    }
+
+    private void ListSort(List<int> tempList)
+    {
+        for (int i = 0; i < tempList.Count; i++)
+        {
+            Sorting(tempList, i, i-1);
+        }
+        Debug.Log("TempList");
+
+    }
+
+    void Sorting(List<int> tempList, int a, int b)
+    {
+
+        if (a < 0 || b < 0)
+        {
+            return;
+        }
+        string num = "";
+        foreach (int i in tempList)
+        {
+            num += i + ",";
+        }
+        Debug.Log(num);
+        if (tempList[b] > tempList[a])
+        {
+            int temp = tempList[b];
+            tempList[b] = tempList[a];
+            tempList[a] = temp;
+            Sorting(tempList, a-1, b-1);
+        }
+        else
+        {
+            return;
+        }
+
     }
     #endregion
 }
